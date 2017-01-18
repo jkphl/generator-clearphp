9ca417a3c3728bb99ee9a1caaa2a5d479a7d3905
Joschi Kuphal <joschi@kuphal.net>
2017-01-18 21:54:43 +0100

    Added scrutinizer subgenerator

---
+++ generators/scrutinizer/index.js
@@ -0,0 +1,32 @@
+const Generator = require('../../lib/JkphlGenerator.js');
+const yosay = require('yosay');
+const chalk = require('chalk');
+
+module.exports = class extends Generator {
+    /**
+     * Initialization
+     */
+    initializing() {
+        this.abort = this._hasRunBefore('scrutinizer');
+        if (this.abort) {
+            if (this.options.nested) {
+                this.log(chalk.yellow('Skipping the "scrutinizer" generator as it has already run before ...'));
+            } else {
+                this.log(yosay('YO! I\'m skipping the "scrutinizer" subgenerator of jkphl\'s PHP project kickstarter because it has already been run before!'));
+            }
+        } else if (!this.options.nested) {
+            this.log(yosay('WELCOME! You\'re using the "scrutinizer" subgenerator of jkphl\'s PHP project kickstarter.'));
+        }
+        this.log();
+
+        if (!this.abort) {
+            // Mixin the git generator if it hasn't run before (and this is not a nested call)
+            if (!this.options.nested && !this._hasRunBefore('git')) {
+                console.log('compose with git');
+                this.composeWith(require.resolve('../git'), { nested: true });
+            }
+
+            this.composeWith(require.resolve('./_index.js'));
+        }
+    }
+};
